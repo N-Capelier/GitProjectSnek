215592231c6f5a3df421061a87fb0ac5dc54e3c3
Nicolas Capelier <capelier.nicolas@gmail.com>
2021-02-02 10:32:41 +0100

    Added Unity packages & Custom packages

---
+++ Project SNEK/Assets/Scripts/GameManager.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GameManagement
+{
+    public class GameManager : MonoBehaviour
+    {
+
+
+
+    }
+}
