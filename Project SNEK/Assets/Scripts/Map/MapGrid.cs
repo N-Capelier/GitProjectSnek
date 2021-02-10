c019fd2e031c75c2b45d20d13d829a814a4c8857
Nicolas Capelier <capelier.nicolas@gmail.com>
2021-02-10 11:26:16 +0100

    Added map grid & display

---
+++ Project SNEK/Assets/Scripts/Map/MapGrid.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Map
+{
+    public class MapGrid : Singleton<MapGrid>
+    {
+        [Header("Variables")]
+        [SerializeField] int width;
+        [SerializeField] int height;
+        [SerializeField] [Range(0, 10)] float cellSize = 1;
+        int[,] cells;
+
+        [Header("Display")]
+        [SerializeField] bool displayGridLines = false;
+        Clock refreshTimer;
+        float refreshRate = 1f;
+
+        #region Private methods
+
+        private void Awake()
+        {
+            CreateSingleton();
+        }
+
+        private void Start()
+        {
+            cells = new int[width, height];
+
+            refreshTimer = new Clock(refreshRate);
+            refreshTimer.ClockEnded += RefreshLines;
+            RefreshLines();
+        }
+
+
+        void RefreshLines()
+        {
+            if (!displayGridLines)
+                return;
+
+            Vector3 _offset = new Vector3(cellSize, 0, cellSize) * 0.5f;
+
+            for (int x = 0; x < cells.GetLength(0); x++)
+            {
+                for (int z = 0; z < cells.GetLength(1); z++)
+                {
+                    Debug.DrawLine(GetWorldPos(x, z) - _offset, GetWorldPos(x, z + 1) - _offset, Color.white, refreshRate);
+                    Debug.DrawLine(GetWorldPos(x, z) - _offset, GetWorldPos(x + 1, z) - _offset, Color.white, refreshRate);
+                }
+            }
+            Debug.DrawLine(GetWorldPos(0, height) - _offset, GetWorldPos(width, height) - _offset, Color.white, refreshRate);
+            Debug.DrawLine(GetWorldPos(width, 0) - _offset, GetWorldPos(width, height) - _offset, Color.white, refreshRate);
+
+            refreshTimer.SetTime(refreshRate);
+        }
+
+        #endregion
+
+        public Vector3 GetWorldPos(int x, int z)
+        {
+            return new Vector3(x + transform.position.x, 0 + transform.position.y, z + transform.position.z) * cellSize;
+        }
+    }
+}
