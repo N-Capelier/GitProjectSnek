3dc06515bb9f034f25ebc250a991836cdf1ed46b
Corentin <job.corentin@outlook.fr>
2021-03-07 00:50:25 +0100

    Added Destroyable Wall Behaviour and FX

--- Project SNEK/Assets/Scripts/LD/WallBehaviour.cs
+++ Project SNEK/Assets/Scripts/LD/WallBehaviour.cs
@@ -7,12 +7,31 @@ namespace Wall
 {
     public class WallBehaviour : MonoBehaviour
     {
+        [SerializeField] bool isDestroyable;
+        [SerializeField] GameObject renderer;
+        [SerializeField] ParticleSystem fx;
         private void OnTriggerEnter(Collider other)
         {
-            if(other.gameObject.layer == LayerMask.NameToLayer("PlayerController"))
+
+            if (other.gameObject.layer == LayerMask.NameToLayer("PlayerController"))
             {
                 PlayerManager.Instance.currentController.Death();
             }
+            else if (other.gameObject.layer == LayerMask.NameToLayer("Attack") && isDestroyable == true)
+            {
+                StartCoroutine(GetDestroyed());
+            }
+        }
+        public IEnumerator GetDestroyed()
+        {
+            gameObject.GetComponent<BoxCollider>().enabled = false;
+            renderer.SetActive(false);
+            if(fx != null)
+            {
+                fx.Play();
+                yield return new WaitForSeconds(fx.main.duration);
+            }
+            Destroy(gameObject);
         }
     }
 }
