17a0d4f2f18f4843a719c8646b6140be799415ef
Arthur SPELLANI <a.spellani@rubika-edu.com>
2021-03-07 11:21:01 +0100

    Add GloutonBehaviour + prefab

---
+++ Project SNEK/Assets/Scripts/Enemy/Glouton/PoisonSpawner.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PoisonSpawner : MonoBehaviour
+{
+    public float timeToSpawn;
+    public GameObject explosion;
+    public GameObject vomi;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(SpawnPoison());
+    }
+
+    IEnumerator SpawnPoison()
+    {
+        yield return new WaitForSeconds(timeToSpawn);
+        Instantiate(explosion, transform.position, Quaternion.identity);
+        yield return new WaitForSeconds(0.2f);
+        Instantiate(vomi, transform.position, Quaternion.identity);
+        Destroy(gameObject);
+    }
+}
