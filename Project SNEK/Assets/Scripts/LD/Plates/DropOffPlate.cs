bada129c754740663c633829b6c934d9206a4ea9
Charles Bentein <c.bentein@rubika-edu.com>
2021-03-18 18:10:38 +0100

    Started Drop-off plate behavior

---
+++ Project SNEK/Assets/Scripts/LD/Plates/DropOffPlate.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Player;
+using System;
+
+namespace Plates
+{
+	public class DropOffPlate : PlateBase
+	{
+		#region Variables
+		bool hasSpirit;
+		GameObject droppedSpirit;
+		#endregion
+
+		// Start is called before the first frame update
+		void Start()
+		{
+			hasSpirit = false;
+		}
+
+        private void OnTriggerEnter(Collider other)
+        {
+			if(other.gameObject.layer == LayerMask.NameToLayer("PlayerController") && !hasSpirit)
+            {
+				CheckActivation();
+				TakeSpirit();
+            }
+
+		}
+
+        private void TakeSpirit()
+        {
+			hasSpirit = true;
+			//Spawn and/or move sprit object
+        }
+
+        private void OnDisable()
+        {
+			droppedSpirit = null;
+        }
+    }
+}
+
