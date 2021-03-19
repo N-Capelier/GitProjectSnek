209c8509bd0f2ca3cd42b5887accd575d7f82026
Arthur SPELLANI <a.spellani@rubika-edu.com>
2021-03-19 23:08:28 +0100

    Started attack behaviour on Mouchounoia

--- Project SNEK/Assets/Scripts/Enemy/Mouchou Variant/MouchouParanoiaBehaviour.cs
+++ Project SNEK/Assets/Scripts/Enemy/Mouchou Variant/MouchouParanoiaBehaviour.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Player;
+using Map;
 
 namespace Enemy
 {
@@ -12,34 +14,119 @@ namespace Enemy
         public GameObject poof;
         bool hadPoof = false;
         bool hadPoofed = false;
+        bool isAttacking = false;
+        bool canAttack = true;
         public GameObject renderGo;
+
+        [Space]
+        float distanceToPlayer;
+        Vector2 directionToPlayer;
+
+        private void Start()
+        {
+            
+        }
 
         private void Update()
         {
-            if(mBm.isMoving == false)
+            if (isAttacking == false)
             {
-                renderGo.GetComponentInChildren<MeshFilter>().mesh = meshes[1];
-                renderGo.GetComponentInChildren<MeshRenderer>().material = mat[1];
-                hadPoof = false;
-                renderGo.transform.rotation = Quaternion.Euler(0, 0, 0);
-                if (hadPoofed == false)
+                if (mBm.isMoving == false)
                 {
-                    Instantiate(poof, transform.position, Quaternion.identity);
-                    hadPoofed = true;
+                    TransformIntoProp();
                 }
+                else if (mBm.isMoving)
+                {
+                    TransformIntoMouchou();
+                }                
             }
-            else if (mBm.isMoving)
+
+            if ((mBm.currentDirection == MouchouDirection.Up && transform.position.z >= MapGrid.Instance.GetWorldPos(0, (int)mBm.nextNode.z).z) ||
+                (mBm.currentDirection == MouchouDirection.Right && transform.position.x >= MapGrid.Instance.GetWorldPos((int)mBm.nextNode.x, 0).x) ||
+                (mBm.currentDirection == MouchouDirection.Down && transform.position.z <= MapGrid.Instance.GetWorldPos(0, (int)mBm.nextNode.z).z) ||
+                (mBm.currentDirection == MouchouDirection.Left && transform.position.x <= MapGrid.Instance.GetWorldPos((int)mBm.nextNode.x, 0).x))
             {
-                renderGo.GetComponentInChildren<MeshFilter>().mesh = meshes[0];
-                renderGo.GetComponentInChildren<MeshRenderer>().material = mat[0];
-                hadPoofed = false;
-                if (hadPoof == false)
+                canAttack = true;
+            }
+
+            if (PlayerManager.Instance.currentController != null)
+            {
+                distanceToPlayer = Vector3.Distance(PlayerManager.Instance.currentController.transform.position, gameObject.GetComponentInParent<EnemyStats>().transform.position);
+
+                if (distanceToPlayer < 4)
                 {
-                    Instantiate(poof, transform.position, Quaternion.identity);
-                    hadPoof = true;
+                    AttackPlayer();
                 }
+                else
+                {
+                    isAttacking = false;
+                    mBm.canMove = true;
+                }
+            }         
+        }
+
+        float directionX;
+        float directionZ;
+
+        void AttackPlayer()
+        {
+            TransformIntoMouchou();
+            directionX = transform.position.x - PlayerManager.Instance.currentController.transform.position.x;
+            directionZ = transform.position.z - PlayerManager.Instance.currentController.transform.position.z;
+            isAttacking = true;
+            mBm.canMove = false;
+            directionToPlayer = new Vector2(directionX, directionZ).normalized;
+
+            if(directionToPlayer.x > (Mathf.Sqrt(2)*0.5f))
+            {
+                mBm.currentDirection = MouchouDirection.Left;
+            }
+            else if(directionToPlayer.x < (-Mathf.Sqrt(2) * 0.5f))
+            {
+                mBm.currentDirection = MouchouDirection.Right;
+            }
+            else if(directionToPlayer.y > (Mathf.Sqrt(2) * 0.5f))
+            {
+                mBm.currentDirection = MouchouDirection.Down;
+            }
+            else
+            {
+                mBm.currentDirection = MouchouDirection.Up;
+            }
+
+            if (canAttack == true)
+            {
+                mBm.GetNextNode();
+                mBm.UpdateMovementDash();
+                canAttack = false;
+            }           
+        }
+
+        void TransformIntoProp()
+        {
+            renderGo.GetComponentInChildren<MeshFilter>().mesh = meshes[1];
+            renderGo.GetComponentInChildren<MeshRenderer>().material = mat[1];
+            hadPoof = false;
+            renderGo.transform.rotation = Quaternion.Euler(0, 0, 0);
+            if (hadPoofed == false)
+            {
+                Instantiate(poof, transform.position, Quaternion.identity);
+                hadPoofed = true;
             }
         }
+
+        void TransformIntoMouchou()
+        {
+            renderGo.GetComponentInChildren<MeshFilter>().mesh = meshes[0];
+            renderGo.GetComponentInChildren<MeshRenderer>().material = mat[0];
+            hadPoofed = false;
+            if (hadPoof == false)
+            {
+                Instantiate(poof, transform.position, Quaternion.identity);
+                hadPoof = true;
+            }
+        }
+
     }
 }
 
--- Project SNEK/Assets/Scripts/Enemy/MouchouBase/MouchouBaseMovement.cs
+++ Project SNEK/Assets/Scripts/Enemy/MouchouBase/MouchouBaseMovement.cs
@@ -66,7 +66,7 @@ namespace Enemy
 
         public void UpdateMovementDash()
         {
-            rb.velocity = (nextNode - currentNode) * (stats.moveSpeed * 2);
+            rb.velocity = (nextNode - currentNode) * (stats.moveSpeed * 3);
             anim.SetBool("isMoving", true);
             isMoving = true;
         }
