fefb465c71994efcf817fc586861975ee919d93a
Nicolas Capelier <capelier.nicolas@gmail.com>
2021-04-09 15:02:44 +0200

    Temp : Removed stats applying in run

--- Project SNEK/Assets/Scripts/Player/PlayerController.cs
+++ Project SNEK/Assets/Scripts/Player/PlayerController.cs
@@ -61,9 +61,9 @@ namespace Player.Controller
         public void Init(int _bonusHP, int _bonusRange, int _bonusPower)
         {
             currentHP = baseHP + _bonusHP;
-            playerRunAttack.rangeBonus += _bonusRange;
-            playerRunAttack.rangeBonusOffSet += _bonusRange * 0.5f;
-            playerRunAttack.attackDamages += _bonusPower;
+            //playerRunAttack.rangeBonus += _bonusRange;
+            //playerRunAttack.rangeBonusOffSet += _bonusRange * 0.5f;
+            //playerRunAttack.attackDamages += _bonusPower;
         }
 
         public void Death()
