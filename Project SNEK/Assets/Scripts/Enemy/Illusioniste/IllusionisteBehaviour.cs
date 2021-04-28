9673d57c95ae5bcfb1fe2a3ce20c23f3b53772a1
Arthur SPELLANI <a.spellani@rubika-edu.com>
2021-04-28 14:09:47 +0200

    Started Illusionist

---
+++ Project SNEK/Assets/Scripts/Enemy/Illusioniste/IllusionisteBehaviour.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Enemy
+{
+    public class IllusionisteBehaviour : MonoBehaviour
+    {
+        EnemyStats stats;
+        IllusionisteMovement movement;
+        public GameObject clone;
+        public int cloneNumber;
+        public GameObject[] clonesList;
+
+        private void Start()
+        {
+            clonesList = new GameObject[cloneNumber];
+            stats = GetComponent<EnemyStats>();
+            movement = GetComponent<IllusionisteMovement>();
+            stats.attackClock.ClockEnded += OnshouldAttack;
+            movement.InstantiateClones();
+        }
+
+
+        void OnshouldAttack()
+        {
+            Debug.Log("Illu_ATK");
+        }
+
+        void InstantiateClones()
+        {
+            
+        }
+
+    }
+}
