94b15c675eb123e9d9214a30f746259ba5b014bc
Corentin <job.corentin@outlook.fr>
2021-05-19 21:50:12 +0200

    Added Face Shader Integration WIP

--- Project SNEK/Assets/Scripts/Hub/NPCs/NPCFaceManager.cs
+++ Project SNEK/Assets/Scripts/Hub/NPCs/NPCFaceManager.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[System.Serializable]
 public class NPCFaceManager : MonoBehaviour
 {
     /// <summary>
@@ -10,10 +11,15 @@ public class NPCFaceManager : MonoBehaviour
 
     [SerializeField] Material faceMat;
     [SerializeField] Vector2[] eyesList;
-    [SerializeField] Vector2[] mouthList;
+    [SerializeField] Vector2[][] mouthList;
+    [SerializeField] int mouthColumn;
 
-    public void RandomizeMouth()
+
+    public void RandomizeMouth(int face)
     {
-        faceMat.SetVector("Mouth", mouthList[Random.Range(0, mouthList.Length + 1)]);
+        faceMat.SetVector("Mouth", mouthList[face][Random.Range(0, mouthList.Length + 1)]);
     }
+
+    /// Le int Face représente l'expression qu'adopte le personnage,
+    /// Ensuite on randomise la bouche de cette expression pour garder une cohérence.
 }
