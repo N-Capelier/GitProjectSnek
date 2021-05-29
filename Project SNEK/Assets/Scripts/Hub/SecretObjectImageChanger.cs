ad657e68652fc85655c149a3d6fffd6ba5310506
Arthur <a.spellani@rubika-edu.com>
2021-05-29 14:18:09 +0200

    Added SecretObjectImageChanger for level selection

---
+++ Project SNEK/Assets/Scripts/Hub/SecretObjectImageChanger.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Saving;
+
+public class SecretObjectImageChanger : MonoBehaviour
+{
+    public int iD;
+    public Sprite spriteToChange;
+
+    private void Start()
+    {
+        switch (iD)
+        {
+            case 0:
+                return;
+            case 1:
+                if(SaveManager.Instance.state.secretObject_1 >= 1)
+                {
+                    GetComponent<Image>().sprite = spriteToChange;
+                }
+                return;
+            case 2:
+                if (SaveManager.Instance.state.secretObject_2 >= 1)
+                {
+                    GetComponent<Image>().sprite = spriteToChange;
+                }
+                return;
+            case 3:
+                if (SaveManager.Instance.state.secretObject_3 >= 1)
+                {
+                    GetComponent<Image>().sprite = spriteToChange;
+                }
+                return;
+        }
+    }
+
+    private void Update()
+    {
+        if(GetComponent<Image>().sprite != spriteToChange)
+        {
+            switch (iD)
+            {
+                case 0:
+                    return;
+                case 1:
+                    if (SaveManager.Instance.state.secretObject_1 >= 1)
+                    {
+                        GetComponent<Image>().sprite = spriteToChange;
+                    }
+                    return;
+                case 2:
+                    if (SaveManager.Instance.state.secretObject_2 >= 1)
+                    {
+                        GetComponent<Image>().sprite = spriteToChange;
+                    }
+                    return;
+                case 3:
+                    if (SaveManager.Instance.state.secretObject_3 >= 1)
+                    {
+                        GetComponent<Image>().sprite = spriteToChange;
+                    }
+                    return;
+            }
+        }        
+    }
+}
