99c6921ea80f3af7d6198cc60102b1c8f22d7265
Thomas Depraz <t.deprazdepland@rubika-edu.com>
2021-07-09 16:01:04 +0200

    Added checkpoints to Progress Bar + Fixed Progess Bar bugs

---
+++ Project SNEK/Assets/Scripts/Map/CheckpointUI.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Thomas
+/// </summary>
+
+namespace Map
+{
+    public class CheckpointUI : MonoBehaviour
+    {
+        [HideInInspector] public bool check;
+        public RectTransform rectTransform;
+    }
+
+}
--- Project SNEK/Assets/Scripts/Map/LevelProgressUI.cs
+++ Project SNEK/Assets/Scripts/Map/LevelProgressUI.cs
@@ -21,12 +21,24 @@ namespace Map
 
         float currentProgression;
 
+        [Header("Checkpoints")]
+        [SerializeField] List<int> checkpointPositions;
+        [SerializeField] List<CheckpointUI> checkpoints;
+
+        [Header("Cinematic")]
+        [SerializeField] Image cinematicImage;
+        [SerializeField] int minBound;
+        [SerializeField] int maxBound;
+
+
         void Start()
         {
             GameManager.Instance.uiHandler.levelProgressUI = this;
             
             fillMask.fillAmount = 0;
             player = PlayerManager.Instance.currentController;
+
+            Initcheckpoints();
         }
 
         void Update()
@@ -39,15 +51,49 @@ namespace Map
             {
                 currentProgression = player.transform.position.z / levelLength;
 
-                if(currentProgression > fillMask.fillAmount)
+                if(currentProgression != fillMask.fillAmount)
                 {
                     fillMask.fillAmount = currentProgression;
                     headSlider.value = currentProgression;
                 }
-                else
+                CheckpointFeedback();
+            }
+
+        }
+
+        void Initcheckpoints()
+        {
+            for (int i = 0; i < checkpointPositions.Count; i++)
+            {
+                float value = (float)checkpointPositions[i] / (float)levelLength;
+                print(value);
+
+                checkpoints[i].rectTransform.anchorMin = new Vector2(0, value);
+                checkpoints[i].rectTransform.anchorMax = new Vector2(1,value); 
+            }
+
+            cinematicImage.rectTransform.anchorMin = new Vector2(0, (float)minBound / (float)levelLength);
+            cinematicImage.rectTransform.anchorMax = new Vector2(1, (float)maxBound / (float)levelLength);
+        }
+
+
+        void CheckpointFeedback()
+        {
+            for (int i = 0; i < checkpointPositions.Count; i++)
+            {
+                if(!checkpoints[i].check)
                 {
-                    fillMask.fillAmount = Mathf.Lerp(fillMask.fillAmount, currentProgression, Time.deltaTime);
+                    if(player.transform.position.z < checkpointPositions[i])
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        checkpoints[i].check = true;
+                        checkpoints[i].gameObject.SetActive(false);
+                    }
                 }
+               ;
             }
         }
     }
