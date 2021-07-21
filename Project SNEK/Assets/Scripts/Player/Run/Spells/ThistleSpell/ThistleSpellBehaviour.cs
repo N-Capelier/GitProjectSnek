defcd900736a90dda34bfc61372abf31938cde8b
Corentin <job.corentin@outlook.fr>
2021-07-21 16:51:55 +0200

    Added Thistle Spell WIP

---
+++ Project SNEK/Assets/Scripts/Player/Run/Spells/ThistleSpell/ThistleSpellBehaviour.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Player.Spells
+{
+    public class ThistleSpellBehaviour : MonoBehaviour
+    {
+        private void OnTriggerStay2D(Collider2D collision)
+        {
+            if (collision.gameObject.layer == LayerMask.NameToLayer("Beam"))
+            {
+                //Détruire l'objet;
+            }
+        }
+    }
+}
+
