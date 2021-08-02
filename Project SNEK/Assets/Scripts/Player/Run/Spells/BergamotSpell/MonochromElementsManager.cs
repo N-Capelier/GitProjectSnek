ae434477b882dbe5b43329f139081082d9cf0a47
Thomas Depraz <t.deprazdepland@rubika-edu.com>
2021-08-02 15:53:19 +0200

    Added Bergamot Spell (Feedback missing)

---
+++ Project SNEK/Assets/Scripts/Player/Run/Spells/BergamotSpell/MonochromElementsManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Player.Spells
+{
+    public class MonochromElementsManager : Singleton<MonochromElementsManager>
+    {
+        private void Awake()
+        {
+            CreateSingleton(false);
+        }
+
+        public List<MonochromElement> elements = new List<MonochromElement>();
+
+    }
+}
+
+
